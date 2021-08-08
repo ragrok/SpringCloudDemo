36269a9d6cadfaf7b5f8af50e0a5a38d65d9d00b
wind.li <wind.li@yeahmobi.com>
2021-08-08 18:38:46 +0800

    修改项目内容

---
+++ SpringCloud-netty-demo/Springcloud-netty-client/src/main/java/cn/com/github/client/SpringCloudDemoNettyClientApplication.java
@@ -0,0 +1,13 @@
+package cn.com.github.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudDemoNettyClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDemoNettyClientApplication.class, args);
+    }
+
+}
---
+++ SpringCloud-netty-demo/Springcloud-netty-client/src/test/java/cn/com/github/client/SpringCloudDemoNettyClientApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.github.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudDemoNettyClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
