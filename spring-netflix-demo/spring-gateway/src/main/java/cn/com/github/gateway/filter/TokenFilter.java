e299cc57450402bfdb77443b7861c80c1f4be546
wind.li <wind.li@yeahmobi.com>
2021-09-07 13:48:17 +0800

    新增事务Demo

--- spring-netflix-demo/spring-gateway/src/main/java/cn/com/github/gateway/filter/TokenFilter.java
+++ spring-netflix-demo/spring-gateway/src/main/java/cn/com/github/gateway/filter/TokenFilter.java
@@ -1,7 +1,5 @@
 package cn.com.github.gateway.filter;
 
-import cn.hutool.json.JSON;
-import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
