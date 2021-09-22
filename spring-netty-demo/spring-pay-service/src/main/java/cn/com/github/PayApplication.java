7d4e6f35b29b5de955d5ab4b3cb3b2851d0e3682
windli93 <windli93@outlook.com>
2021-09-22 18:38:35 +0800

    修改部分配置

--- spring-netty-demo/spring-pay-service/src/main/java/cn/com/github/PayApplication.java
+++ spring-netty-demo/spring-pay-service/src/main/java/cn/com/github/PayApplication.java
@@ -2,8 +2,10 @@ package cn.com.github;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableDubbo(scanBasePackages = "cn.com.github.api")
+@SpringBootApplication
 public class PayApplication {
 
     public static void main(String[] args) {
