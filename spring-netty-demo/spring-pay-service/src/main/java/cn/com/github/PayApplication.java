2c4fd8ec280eea578e0d5468b1d6161c1c1506c5
windli93 <windli93@outlook.com>
2021-09-23 12:16:22 +0800

    提交代码到github

--- spring-netty-demo/spring-pay-service/src/main/java/cn/com/github/PayApplication.java
+++ spring-netty-demo/spring-pay-service/src/main/java/cn/com/github/PayApplication.java
@@ -3,9 +3,11 @@ package cn.com.github;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDubbo(scanBasePackages = "cn.com.github.api")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PayApplication {
 
     public static void main(String[] args) {
