3448049824df92f0894ade96eecfeb3240d47c08
windli93 <windli93@outlook.com>
2021-09-23 16:45:34 +0800

    修改部分代码

--- spring-netty-demo/spring-pay-service/src/main/java/cn/com/github/PayApplication.java
+++ spring-netty-demo/spring-pay-service/src/main/java/cn/com/github/PayApplication.java
@@ -4,12 +4,10 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
 
 @EnableDubbo(scanBasePackages = "cn.com.github.impl")
 @SpringBootApplication
 @EnableDiscoveryClient
-@Configuration
 public class PayApplication {
 
     public static void main(String[] args) {
